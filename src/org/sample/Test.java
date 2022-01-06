204a4771fd9ce75390ff6f89361f1ca42827a270
GowdamanSundaram <gowdamansundaram@gmail.com>
2022-01-06 17:27:58 +0530

    test

---
+++ src/org/sample/Test.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class Test {
+	public static void main(String[] args) {
+		String a="Gowdaman";
+		if (a.contains("Gowd")) {
+			System.out.println("Name:"+a);
+			
+		}
+	}
+
+}
